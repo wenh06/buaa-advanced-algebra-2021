e9633b714e30d598c30ca3de1efb585e31349130
WEN Hao <wenh06@gmail.com>
2022-05-21 23:56:25 +0800

    add formatting.sh

--- collect_assignments.py
+++ collect_assignments.py
@@ -1,8 +1,9 @@
 """
 """
 
-import os, sys, io, time
-import re, base64
+import os
+import time
+import re
 import email
 from email.header import decode_header
 import imaplib
@@ -10,9 +11,7 @@ import getpass
 import warnings
 from typing import NoReturn
 
-import chardet
 import pandas as pd
-from PIL import Image
 
 
 _EMAIL = "wenh06@buaa.edu.cn"
@@ -21,18 +20,20 @@ _PORT = 993  # 接收端口
 _SUBJECT_PATTERN = "^(?P<id>[\d]{8})(?:[\s]*[\-_][\s]*)?(?P<name>[\u4e00-\u9FFF]{0,10})(?:[\s]*[\-_][\s]*)?第(?P<no>[\d一二三四五六七八九十]{1,2})次作业"
 # _BASE64_UTF8_PATTERN = "^=\?(?:utf|UTF)\-8\?(?:b|B)\?(?P<base64_string>.*)\?=$"
 
-_zh2num = {k:v for k,v in zip("一二三四五六七八九十", range(1,11))}
+_zh2num = {k: v for k, v in zip("一二三四五六七八九十", range(1, 11))}
 for k in "一二三四五六七八九":
     _zh2num[f"十{k}"] = 10 + _zh2num[k]
 
 _PWD = os.path.abspath(os.path.curdir)
 _STATS_FILE = os.path.join(_PWD, "统计.csv")
-_COLS = ["学号", "姓名",] + [f"第{i}次作业" for i in range(1,20)]
+_COLS = [
+    "学号",
+    "姓名",
+] + [f"第{i}次作业" for i in range(1, 20)]
 
 
 def collect() -> NoReturn:
-    """
-    """
+    """ """
     start = time.time()
     # connect to the server and go to its inbox
     mail = imaplib.IMAP4_SSL(_SERVER, _PORT)
@@ -79,7 +80,9 @@ def collect() -> NoReturn:
                     except:
                         pass
                 try:
-                    From_email = re.findall("\<(?P<email>[\w\d\_\-]+@[\w\d\.\_\-]+)\>", msg.get("From"))[0]
+                    From_email = re.findall(
+                        "\<(?P<email>[\w\d\_\-]+@[\w\d\.\_\-]+)\>", msg.get("From")
+                    )[0]
                 except:
                     From_email = ""
 
@@ -93,62 +96,89 @@ def collect() -> NoReturn:
                         pass
                 if isinstance(subject, str):
                     subject = re.sub("高代|高等代数", "", subject)
-                if not isinstance(subject, str) or not re.search(_SUBJECT_PATTERN, subject):
+                if not isinstance(subject, str) or not re.search(
+                    _SUBJECT_PATTERN, subject
+                ):
                     continue
-                subject_content = list(re.finditer(_SUBJECT_PATTERN, subject))[0].groupdict()
+                subject_content = list(re.finditer(_SUBJECT_PATTERN, subject))[
+                    0
+                ].groupdict()
                 student_id = int(subject_content["id"])
                 student_name = subject_content["name"]
                 assignment_no = subject_content["no"]
                 assignment_no = _zh2num.get(assignment_no, assignment_no)
 
-                if student_name == "" and From!= "" and isChinese(From, strict=True):
+                if student_name == "" and From != "" and isChinese(From, strict=True):
                     student_name = From
 
-                df_assignment = df_stats[(df_stats["学号"]==student_id) & (~df_stats[f"第{assignment_no}次作业"].isna())]
+                df_assignment = df_stats[
+                    (df_stats["学号"] == student_id)
+                    & (~df_stats[f"第{assignment_no}次作业"].isna())
+                ]
 
                 if not df_assignment.empty:
-                    print(f"{student_name}({student_id}) 第{assignment_no}次作业 already collected.")
+                    print(
+                        f"{student_name}({student_id}) 第{assignment_no}次作业 already collected."
+                    )
                     print("Continue.")
-                    print("="*100)
+                    print("=" * 100)
                     continue
 
                 print("Subject:", subject)
                 print("From:", From)
                 print("Email address:", From_email)
 
-                folder_name = os.path.join(_PWD, "attachments", f"{student_id}-{student_name}")
+                folder_name = os.path.join(
+                    _PWD, "attachments", f"{student_id}-{student_name}"
+                )
                 save_folder_name = os.path.join(folder_name, f"第{assignment_no}次作业")
                 os.makedirs(save_folder_name, exist_ok=True)
 
                 if student_id not in df_stats.学号.values.tolist():
-                    df_stats = df_stats.append({
-                    "学号": student_id,
-                    "姓名": student_name,
-                    f"第{assignment_no}次作业": -1,
-                }, ignore_index=True)
+                    df_stats = df_stats.append(
+                        {
+                            "学号": student_id,
+                            "姓名": student_name,
+                            f"第{assignment_no}次作业": -1,
+                        },
+                        ignore_index=True,
+                    )
                 else:
-                    df_stats.loc[df_stats["学号"]==student_id, f"第{assignment_no}次作业"] = -1
+                    df_stats.loc[
+                        df_stats["学号"] == student_id, f"第{assignment_no}次作业"
+                    ] = -1
                 newly_collected += 1
-                    
+
                 if not msg.is_multipart():
                     continue
-                
+
                 msg_content_type = msg.get_content_type()
-                
-                if msg_content_type in ["multipart/mixed", "multipart/related"]:  # with attachments or inline image
+
+                if msg_content_type in [
+                    "multipart/mixed",
+                    "multipart/related",
+                ]:  # with attachments or inline image
                     print("with attachments or inline images")
                     for part in msg.get_payload():
                         content_type = part.get_content_type()
                         content_disposition = str(part.get("Content-Disposition"))
-                        content_transfer_encoding = part.get("Content-Transfer-Encoding")
-                        if content_type == "text/plain" and "attachment" not in content_disposition:
+                        content_transfer_encoding = part.get(
+                            "Content-Transfer-Encoding"
+                        )
+                        if (
+                            content_type == "text/plain"
+                            and "attachment" not in content_disposition
+                        ):
                             try:
                                 body = part.get_payload(decode=True).decode()
                             except:
                                 body = ""
                             # print text/plain emails and skip attachments
                             print(body)
-                        elif "attachment" in content_disposition or content_type.startswith("image"):
+                        elif (
+                            "attachment" in content_disposition
+                            or content_type.startswith("image")
+                        ):
                             # download attachment
                             filename, encoding = decode_header(part.get_filename())[0]
                             if encoding:
@@ -162,18 +192,21 @@ def collect() -> NoReturn:
                                     f.write(part.get_payload(decode=True))
                                     new_attachment += 1
                             else:
-                                warnings.warn(f"{student_name}({student_id}) 第{assignment_no}次作业未找到附件或内嵌图片")
+                                warnings.warn(
+                                    f"{student_name}({student_id}) 第{assignment_no}次作业未找到附件或内嵌图片"
+                                )
                 else:
-                    warnings.warn(f"{student_name}({student_id}) 第{assignment_no}次作业未找到附件或内嵌图片")
-                print("="*100)
+                    warnings.warn(
+                        f"{student_name}({student_id}) 第{assignment_no}次作业未找到附件或内嵌图片"
+                    )
+                print("=" * 100)
     df_stats.to_csv(_STATS_FILE, index=False)
     print(f"collection used {time.time()-start:.2f} seconds")
     print(f"新收取作业{newly_collected}份，附件{new_attachment}份")
 
 
-def isChinese(text:str, strict:bool=False) -> bool:
-    """
-    """
+def isChinese(text: str, strict: bool = False) -> bool:
+    """ """
     pattern = "[\u4e00-\u9FFF]"
     if not re.search(pattern, text):  # contains Chinese
         return False
